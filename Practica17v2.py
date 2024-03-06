baa0567c828d3ecb36f46c9221ac59ecc0453bff
FourSoda8545 <root@baticomputadora.ipv6.infinitum.net.mx>
2024-03-06 07:30:57 -0600

    v3

---
+++ Practica17v2.py
@@ -0,0 +1,9 @@
+# Prcatica17v2
+
+suma=3-1+2-1
+for x in range(6):
+	print("x: ",x)
+	print("valor suma : ",suma)
+	suma+=x #suma = suma+x
+	print("nuevo valor suma :",suma)
+	print("-------------------------------------------------------")
